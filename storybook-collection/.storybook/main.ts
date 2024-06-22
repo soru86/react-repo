ebba1ed43d770b106850cc26a5a3199bc4583a2e
Saurabh Agrawal <saurabh.agrawal.86@outlook.com>
2024-06-22 17:05:14 +0800

    first commit

---
+++ storybook-collection/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-themes",
+    "storybook-addon-mock",
+    "storybook-addon-apollo-client",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
